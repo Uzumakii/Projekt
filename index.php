42a45ea2172dd7dea183cbeddbd2372a4b703abb
Krzysztof Pazdur <pazdurk@gmail.com>
2013-03-17 21:48:23 +0100

    Modyfikacja Szablonu

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?
+<?php
 	#index 
-
+	require("admin/module.template.inc.php");
+	
+	$templates = new Template('./template/');
+	
+	$templates->set_filenames(array("body" => "document.html"));
+	
+	$templates->pparse("body");
 ?>
