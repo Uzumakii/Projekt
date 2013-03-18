aee034630caa6400bb897821ecb188d07993c821
Krzysztof Pazdur <pazdurk@gmail.com>
2013-03-18 19:50:47 +0100

    Silnik
    Buduję silnik

---
+++ Admin/module.reader.inc.php
@@ -0,0 +1,48 @@
+﻿<?php
+
+	class Reader
+	{
+		public $path = './';
+		
+		public $folder = 'files/';
+			
+		private $index_folderow = array();
+		
+		public $ilemang = 0;
+		
+		public $mangi = array();
+		 		
+			function __construct($directory = './',$folder = 'files/')
+			{
+					$this->path = $directory;
+					$this->folder = $folder;
+					Reader::IleMangWkatalogu();
+			}
+		
+		public function IleMangWkatalogu()
+		{
+			$this->mangi = glob($this->path.$this->folder.'*',GLOB_ONLYDIR);
+			$this->ilemang = count($this->mangi);
+			
+			
+		}
+		public function GenerujIndex()
+		{
+			if($this->ilemang != 0)
+			{
+				for($i=0; $i < $this->ilemang; $i++)
+				{
+					$rozdzialow = count(glob($this->mangi[$i].'/*',GLOB_ONLYDIR));
+					 $struktura[] = array(
+									"nazwa" =>$this->mangi[$i],
+									"md5" => md5($this->mangi[$i]),
+									"rozdz" => ($rozdzialow)
+								 );
+				}
+			}
+			var_dump($struktura);
+		}
+	}
+
+
+?>
--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 	#index 
 	require("admin/module.template.inc.php");
-	
+	require("admin/module.reader.inc.php");
 	$templates = new Template('./template/');
-	
+	$reader = new Reader();
 	$templates->set_filenames(array("body" => "document.html"));
+	
+	$reader->GenerujIndex();
+	
+	var_dump($reader);	
 	
 	$templates->pparse("body");
 ?>
